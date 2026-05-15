6dae41d4b55f15aa512e17905fbc571a9552af2e
dutchlearningforsgod <harry.nak256@gmail.com>
2026-05-15 19:31:38 +0200

    Create lesson1.js

---
+++ src/src/data/lesson1.js
@@ -0,0 +1,19 @@
+const lesson1 = {
+  title: "Greetings",
+  words: [
+    {
+      dutch: "Hallo",
+      english: "Hello"
+    },
+    {
+      dutch: "Dank je",
+      english: "Thank you"
+    },
+    {
+      dutch: "Tot ziens",
+      english: "Goodbye"
+    }
+  ]
+}
+
+export default lesson1
