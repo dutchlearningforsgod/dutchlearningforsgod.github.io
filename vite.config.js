46dc45fed4cd3a5aaafaec21aadb134e65d15a74
Martijn <martijnswart01@gmail.com>
2026-05-16 14:10:02 +0200

    Updated build

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',   // IMPORTANT FIX
-<<<<<<< HEAD
 })
-=======
-})
->>>>>>> ef1746a0ecb1b6d354c7033365c0832538e6afb9
